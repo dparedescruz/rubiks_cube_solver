cbe3358091ead47afcb173dd37b0a1d6ec26fe3f
Jonathan Champeth <jonbrah@Jonathans-MacBook-Air-98.local>
2023-04-29 10:47:59 -0700

    cmake

---
+++ CMakeFiles/run.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/run.dir/src/Cube.cpp.o"
+  "CMakeFiles/run.dir/src/Cube.cpp.o.d"
+  "CMakeFiles/run.dir/src/main.cpp.o"
+  "CMakeFiles/run.dir/src/main.cpp.o.d"
+  "run"
+  "run.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/run.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
